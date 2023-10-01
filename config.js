5168b1bdf021dffc30f072f683972bbf156e6c9a
Foysal Md <foysalmd@Foysals-MacBook.cust.communityfibre.co.uk>
2023-10-01 22:12:12 +0100

    update config file

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_wx8B0d94G', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '76cjkq2j8fnm5ecr3lorbpp6bn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
